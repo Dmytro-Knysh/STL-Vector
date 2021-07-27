50a13413e5d14ffc7c5a824da57f9792c59fbade
Dmytro-Knysh <87389774+Dmytro-Knysh@users.noreply.github.com>
2021-07-27 10:10:36 +0300

    Update and rename ConsoleApplication2.cpp to STL_2.cpp

--- STL_2.cpp
+++ STL_2.cpp
@@ -9,11 +9,11 @@ int main()
 	int k, t;
 	float x;
 	vector<float>a; vector<float>b;
-	cout << "Введіть k: ";
+	cout << "Input k: ";
 	cin >> k;
 	for (int i = 0; i < k; i++)
 	{
-		cout << "Введіть a["<<i<<"]: ";
+		cout << "Input a["<<i<<"]: ";
 		cin >> x;
 		a.push_back(x);
 	}
@@ -25,12 +25,12 @@ int main()
 			b.push_back(a[i]);
 		}
 	}
-	cout << "Вектор A: ";
+	cout << "Vector A: ";
 	for (int i = 0; i < a.size(); i++)
 	{
 		cout << a[i] << " ";
 	}
-	cout << endl << "Вектор B: ";
+	cout << endl << "Vector B: ";
 	for (int i = 0; i < b.size(); i++)
 	{
 		cout << b[i] << " ";
